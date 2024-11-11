9403f49ac532ec626d828bcc71366d356b5a2a6d
anojpatil <itplmobilede@infiteai.in>
2024-11-11 12:36:00 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,24 @@
+const heading = React.createElement(
+    "div",
+    { id: "headers"},
+    [
+        React.createElement("div",{key:"div1"},
+            [
+                React.createElement('h1', { key: 1 }, "this is h1 from react div1"), 
+                React.createElement('h3', { key: 2 }, "this is h3 from react div1")
+            ]
+        ),
+        React.createElement("div",{key:"div2"},
+            [
+                React.createElement('h1', { key: 1 }, "this is h1 from react div2"), 
+                React.createElement('h3', { key: 2 }, "this is h3 from react div2")
+            ]
+        )
+    ]
+);
+
+console.log(heading);
+console.log(React);
+const root = ReactDOM.createRoot(document.getElementsByClassName("root")[0]);
+root.render(heading);
+console.log(root);
